f1375e3cfe01aaf4dfb55927ceb8bb25de7404f9
Margarita555 <a_rita@ukr.net>
2022-03-28 09:22:11 +0300

    added files

---
+++ src/scripts/main.ts
@@ -0,0 +1,22 @@
+// import sum from "./module/sum.js";
+// import blockOne from "./module/block_one.ts";
+const sum = (a: number, b: number) => a + b;
+console.log(sum(2, 10));
+console.log(sum(3, 7));
+// {
+//   "compilerOptions": {
+//     "target": "es2016",
+
+//     "module": "commonjs",
+
+//     "allowJs": true,
+
+//     "esModuleInterop": true,
+
+//     "forceConsistentCasingInFileNames": true,
+
+//     "strict": true,
+
+//     "skipLibCheck": true
+//   }
+// }
